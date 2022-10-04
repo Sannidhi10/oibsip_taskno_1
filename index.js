566c2624da55e767646483c4ca4743ad39c41f19
Sannidhi <sannidhib25@gmail.com>
2022-10-04 16:07:21 +0530

    oasistask1

---
+++ index.js
@@ -0,0 +1,17 @@
+let screen= document.getElementById('screen');
+buttons= document.querySelectorAll('button');
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText= e.target.innerText;
+        console.log('Button text is',buttonText);
+        if(buttonText=='x'){
+            buttonText= '*';
+            screen.value+= buttonText;
+        
+        }
+        else if (buttonText=='='){
+
+        }
+    })
+
+}
